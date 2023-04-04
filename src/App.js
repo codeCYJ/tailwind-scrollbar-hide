cf84837e3e75d7e2077d772bf976a7bf852e8f5b
codeCYJ <67721584+codeCYJ@users.noreply.github.com>
2023-04-04 11:02:47 +0900

    main

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App scrollbar-hide">
+      <div class="m-5 flex gap-4 overflow-x-auto pb-6 scrollbar-hide">
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
+          Lorem ipsum dolorn
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
