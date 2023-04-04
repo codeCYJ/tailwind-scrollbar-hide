c0bd54e1ee42184339cfd05a622254faedc959cc
codeCYJ <67721584+codeCYJ@users.noreply.github.com>
2023-04-04 11:47:09 +0900

    scrollbar-hide

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Test from "../src/pages/Test"
 
 function App() {
   return (
-    <div className="App scrollbar-hide">
-      <div class="m-5 flex gap-4 overflow-x-auto pb-6 scrollbar-hide">
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-        <div class="w-56 flex-shrink-0 rounded bg-slate-300 p-2">
-          Lorem ipsum dolorn
-        </div>
-      </div>
-    </div>
+    <Test/>
   );
 }
 
---
+++ src/pages/Test.jsx
@@ -0,0 +1,26 @@
+const Test = () => {
+  return (
+    <div className="m-5 flex gap-4 overflow-x-auto pb-6 scrollbar-hide">
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+      <div className="w-56 min-w-fit rounded bg-slate-300 p-2">
+        Lorem ipsum dolorn
+      </div>
+    </div>
+  );
+};
+
+export default Test;
